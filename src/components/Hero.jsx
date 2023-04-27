3c8a1d6681ce5acc42470fb5484c370dfc655668
sidhantb48 <116626112+sidhantb48@users.noreply.github.com>
2023-04-28 03:57:31 +0530

    content updated for download

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,7 +58,7 @@ const Hero = () => {
                 <img src={avatar} alt='avatar' 
                 className='w-60 h-60 md:w-72 md:h-72 object-cover object-top bg-gradient-to-b from-rose-600 rounded-xl pt-5'/>
                 <a 
-                href='/SIDHANT N BASTWADKAR.pdf' 
+                href='/Sidhant Resume.pdf' 
                 download={true} 
                 className="flex items-center justify-center mt-10 bg-gradient-to-r from-rose-600 to-teal-500 text-white py-2 rounded-lg">
                     Resume
